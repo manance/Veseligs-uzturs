b07610cc01a011d4c7cf72223674acd8d2f7f4d0
Dripsters123 <126756209+Eduardsss@users.noreply.github.com>
2026-02-10 09:50:57 +0200

    push 2

--- views/fruits/index.view.php
+++ views/fruits/index.view.php
@@ -1,1 +1,17 @@
-<p>Hello</p>
+<?php ob_start(); ?>
+<h1>Augļi</h1>
+<form>
+    <input name='search_query' value="<?= $_GET['search_query'] ?? "" ?>" />
+    <button>Meklēt</button>
+</form>
+<?php if (count($fruit) == 0) { ?>
+    <p>❌ Nav atrasts neviens ieraksts. 😭 Lūdzu, pamēģini citu vārdu vai frāzi 🐣</p>
+<?php } else { ?>
+    <ul>
+        <?php foreach ($fruits as $fruit) { ?>
+            <li><a href="show?id=<?= $fruit['id'] ?>"><?= htmlspecialchars($fruit['content']) ?></a></li>
+        <?php } ?>
+    </ul>
+<?php } ?>
+<?php $content = ob_get_clean(); ?>
+<?php require "./views/components/layout.php"; ?>
